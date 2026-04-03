1a613a5c1aa0320da33c5b9dc3ef1260978364f8
maxime_alain <69240270+Myoxsis@users.noreply.github.com>
2026-04-03 22:45:33 +0200

    Delete src directory

--- src/clone.js
+++
@@ -1,6 +0,0 @@
-export function cloneValue(value) {
-  if (typeof globalThis.structuredClone === 'function') {
-    return globalThis.structuredClone(value);
-  }
-  return value == null ? value : JSON.parse(JSON.stringify(value));
-}
